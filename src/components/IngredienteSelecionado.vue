4d7b2dd551ace12b4b4ad8268136920e270f59c8
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-03-09 16:26:06 -0300

    feat: create component IngredienteSelecionado

---
+++ src/components/IngredienteSelecionado.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import Tag from './Tag.vue'
+export default {
+  components: { Tag },
+  props: {
+    ingrediente: { type: String, required: true }
+  },
+  data() {
+    return {
+      selecionado: false
+    }
+  }
+}
+</script>
+
+<template>
+  <button
+    class="ingrediente"
+    @click="selecionado = !selecionado"
+    :aria-pressed="selecionado"
+  >
+    <tag :texto="ingrediente" :isAtiva="selecionado"/>
+  </button>
+</template>
+<style scoped>
+.ingrediente {
+  cursor: pointer
+}
+</style>
